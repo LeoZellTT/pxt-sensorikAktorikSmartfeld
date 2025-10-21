bc2354bbb82932ca9695b83399ceecfda20a398d
Leonie Zellweger <leonie.zellweger@ost.ch>
2025-10-21 15:32:14 +0200

    (TEST4)

--- sensoren.ts
+++ sensoren.ts
@@ -18,7 +18,7 @@ namespace smartfeldSensoren {
     //let sen0322 = new SEN0322();
 
     //% group="Ultraschallsensor 101020010"
-    //% block="Distanz in cm (TEST3) |%pin"
+    //% block="Distanz in cm (TEST4) |%pin"
     //% subcategory="Optische Sensoren" weight=100
     export function measureInCentimetersV2(pin: DigitalPin): number {
         let distanceBackup: number = 0;
